2f8a1cdcec5ed61872e09514331c03db80a0fd3e
omerA <oatar717@gmail.com>
2022-05-03 14:58:51 +0300

    Project basic design, and implemented up to the 4th garage function

---
+++ GarageLogic/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageLogic
+{
+    internal abstract class Vehicle
+    {
+        private string m_ModelName;
+        private string m_LicensePlate;
+        private Engine m_Engine;
+        private float m_RemainingEnergy;
+        private List<Wheel> m_Wheels;
+    }
+}
